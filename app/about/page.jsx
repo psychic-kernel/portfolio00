7fdcee00adf7e0b5d96c7a0974e13fe7a247177b
Michael Robins <michaelarobins93@gmail.com>
2024-07-25 16:10:37 -0600

    fixed unescaped apost error

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,13 +1,13 @@
 export const metadata = {
-    title: 'About | Manic Media'
+    title: About | 'Michael A. Robins'
 }
 
 const AboutPage = () => {
     return (
       <main className="relative bloc top-10 text-purple-100">
 
-        <p>Welcome to my portfolio project! 2024 May 11 graduate with an Associate of Applied Science in Software Development. I'm seeking any entry-level positions in Web Development, Web Design, Databases and Data Analytics and cross-platform development.  </p>
-        <p> I built this site using <em className="font-bold text-cyan-300">Next JS</em>, if you haven't noticed already and <em className="font-bold text-cyan-300">deployed with Vercel.</em> I have to say, over the past 2 years I've been working with and studying <em className="font-bold text-cyan-300">JavaScript</em> and its frameworks. I've come to appreciate the the <em>React</em> ecosystem. I've had much hands-on experience developing projects with <em className="font-bold text-cyan-300">Vue</em></p>
+        <p>Welcome to my portfolio project! 2024 May 11 graduate with an Associate of Applied Science in Software Development. I&apos; m seeking any entry-level positions in Web Development, Web Design, Databases and Data Analytics and cross-platform development.  </p>
+        <p> I built this site using <em className="font-bold text-cyan-300">Next JS</em>, if you haven&apos; t noticed already and <em className="font-bold text-cyan-300">deployed with Vercel.</em> I have to say, over the past 2 years I&apos; ve been working with and studying <em className="font-bold text-cyan-300">JavaScript</em> and its frameworks. I&apos; ve come to appreciate the the <em>React</em> ecosystem. I&apos; ve had much hands-on experience developing projects with <em className="font-bold text-cyan-300">Vue</em></p>
         <p></p>
       </main>
     )
